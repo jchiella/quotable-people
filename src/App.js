e1867b450afb75644decb678e2a3797425f2a1fb
James Chiella <james.chiella@gmail.com>
2021-02-09 08:59:11 -0500

    basic app scaffolding

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import { Section, Heading } from 'react-bulma-components';
 
-function App() {
+import 'react-bulma-components/dist/react-bulma-components.min.css';
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Section>
+      <Heading>Hi there!</Heading>
+    </Section>
   );
 }
-
-export default App;
